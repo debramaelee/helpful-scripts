28705b84a362114ea4261be0ff982c66b799ee11
Lee <dxl8371@c02qx3r1fvh5mbp.amer.homedepot.com>
2017-08-23 15:35:47 -0400

    changed delimiters

--- convertText.py
+++ convertText.py
@@ -1,19 +1,26 @@
 # enter original string here between triple quotes
-str0 = """cust_ord_id   STRING  NULLABLE
-last_upd_snsh_id    INTEGER NULLABLE
-tot_ord_retl_amt    FLOAT   NULLABLE
+str0 = """CARR_CD	STRING	NULLABLE	
+Describe this field...
+CARR_PRO_NBR	STRING	NULLABLE	
+Describe this field...
+CRT_TS	TIMESTAMP	NULLABLE	
+Describe this field...
+PRTNR_ID	STRING	NULLABLE	
+Describe this field...
+FRT_BILL_NBR	STRING	NULLABLE	
 """
  
 # remove all unwanted strings and values
-nullVals = ["INTEGER", "STRING", "FLOAT", "NULLABLE", "TIMESTAMP", "Describe this field...", " ", "	"]
+nullVals = ["INTEGER", "STRING", "FLOAT", "NULLABLE", "TIMESTAMP", "BOOLEAN", "Describe this field...", " ", "	"]
 for i in nullVals:
     str0 = str0.replace(i, "")
  
 # replace all paragraph breaks with commas
-newStr = str0. replace("\n", ", ")
+newStr = str0. replace("\n", ",")
+newStr1 = newStr. replace(",,", ", ")
  
 # change all values to uppercase
-finalStr = newStr.upper()
+finalStr = newStr1.upper()
 print '------------------------------------------------------------'
 print finalStr
  
--- getDate.js
+++ getDate.js
@@ -1,10 +1,18 @@
+// gets offset of UTC in minutes
+var offset = new Date().getTimezoneOffset();
 
 // gets current date formatted in YYYY-MM-DD and YYYYMMDD
-var t = new Date(); t.setDate(t.getDate()); 
+var t = new Date(); 
+t.setDate(t.getDate()); 
 t.toISOString().slice(0,10).replace(/-/g,"");
 
 console.log(t);
-console.log(t.toISOString().slice(0,10).replace(/-/g,""))
+console.log(t.toISOString().slice(0,10).replace(/-/g,""));
+
+// new Date().toISOString('en-US', { timeZone: 'EST'});
+// console.log(Date);
+console.log(offset/60);
+
 
 // gets yesterdays date
 // var t = new Date(); t.setDate(t.getDate()-1); t.toISOString().slice(0,10).replace(/-/g,"");
