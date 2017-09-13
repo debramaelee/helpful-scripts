d8e122069ec3b8fe85470c3746b3c6240491b815
Debra Lee <debramaelee@gmail.com>
2017-09-13 11:07:51 -0400

    bug fixes

--- getDate.js
+++ getDate.js
@@ -13,6 +13,12 @@ console.log(t.toISOString().slice(0,10).replace(/-/g,""));
 // console.log(Date);
 console.log(offset/60);
 
-
 // gets yesterdays date
 // var t = new Date(); t.setDate(t.getDate()-1); t.toISOString().slice(0,10).replace(/-/g,"");
+
+// offset UTC time to EST
+var t2 = new Date(); 
+t2.setHours(t2.getHours() - 4); 
+t2.setDate(t2.getDate()); 
+t2.toISOString().slice(0,10);
+console.log(t2);
